f912b247168f78efb96fa97c1dfdfe369c12dcd1
Alondra Villagrana <alondra@MacBook-Air-de-Alondra.local>
2024-07-25 19:55:51 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-df22bf26-4f87-4721-ac26-b8377048c9f5.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
